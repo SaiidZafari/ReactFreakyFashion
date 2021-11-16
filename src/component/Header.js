a7de4ec72d3c72840ab40bb33e288d6ed2251a3e
Saiid Zafari <Saiid.Zafari@bahnhof.se>
2021-11-16 15:38:21 +0100

    Initial Commit

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="Header">
+      <div className="Header-content">
+        <h1> Header </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
